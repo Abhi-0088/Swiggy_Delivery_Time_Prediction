15224b58d1d1859ac90dc0748cb63b08fd9e6c19
Abhi-0088 <abhilashojha351@gmail.com>
2025-01-26 11:54:54 +0530

    First Commit After Initializing the DVC and GIT

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Machine Learning Project to Predict Delivery Time of Swiggy',
+    author='Abhilash',
+    license='',
+)
